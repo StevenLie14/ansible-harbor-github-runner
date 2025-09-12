9d19e1984dd2a57eb760ae821c5048f0de27ea36
SV24-1 <stevenliementha@gmail.com>
2025-09-12 17:24:08 +0700

    feat : install docker

--- run.bat
+++ run.bat
@@ -1,6 +1,8 @@
 @echo off
 set CONTAINER_NAME=ansible
 set INVENTORY=inventory/hosts/hosts.yml
-set PLAYBOOK=roles/github-actions-runner/tasks/main.yml
+set ACTION_PLAYBOOK=roles/github-actions-runner/tasks/main.yml
+set DOCKER_PLAYBOOK=roles/docker/tasks/main.yml
 
-docker exec -it %CONTAINER_NAME% sh -c "ansible-playbook -i %INVENTORY% -b %PLAYBOOK%"
+docker exec -it %CONTAINER_NAME% sh -c "ansible-playbook -i %INVENTORY% -b %DOCKER_PLAYBOOK%"
+@REM docker exec -it %CONTAINER_NAME% sh -c "ansible-playbook -i %INVENTORY% -b %ACTION_PLAYBOOK%"
