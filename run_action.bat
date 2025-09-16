9695a7c3328d393ed25eb0c9716907c24bc0eeed
SV24-1 <stevenliementha@gmail.com>
2025-09-16 13:03:42 +0700

    feat : add ansible to automate set github repo secret

--- run_action.bat
+++ run_action.bat
@@ -4,9 +4,9 @@ set CONTAINER_NAME=ansible
 set INVENTORY=inventory/hosts
 set PLAYBOOK=playbook.yml
 set ACTION_TAGS=runner
-set DOCKER_TAGS=docker
-set HARBOR_TAGS=harbor
+set SECRET_TAGS=secret
 
-docker exec -it ansible sh -c "ansible-playbook -i "%INVENTORY%" "%PLAYBOOK%" --tags %ACTION_TAGS%"
+@REM docker exec -it ansible sh -c "ansible-playbook -i "%INVENTORY%" "%PLAYBOOK%" --tags %ACTION_TAGS%"
+docker exec -it ansible sh -c "ansible-playbook -i "%INVENTORY%" "%PLAYBOOK%" --tags %SECRET_TAGS%"
 
 
